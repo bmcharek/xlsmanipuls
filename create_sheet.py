4919fb95f38b865b0053b8233a8ed90c47ef701d
Besma M'charek <bmcharek@gmail.com>
2013-03-14 11:25:02 +0100

    handling two files, one with relevant adresses on second sheet

--- create_sheet.py
+++ create_sheet.py
@@ -1,29 +1,113 @@
+#!bin/python
+"""
+Klein scriptje om een xls sheet te manipuleren.
+"""
+#TODO: Add specification for sheet number
+import xlrd
+import re
 import xlwt
+a_file = "Meevart_newMaildataMaart13.xls"
+#"Adressen_mailings_wijkenbuurt_jan2013_bestversion.xls"
+book = (xlrd.open_workbook(a_file))
 
-DATA = (("The Essential Calvin and Hobbes", 1988,),
-        ("The Authoritative Calvin and Hobbes", 1990,),
-        ("The Indispensable Calvin and Hobbes", 1992,),
-        ("Attack of the Deranged Mutant Killer Monster Snow Goons", 1992,),
-        ("The Days Are Just Packed", 1993,),
-        ("Homicidal Psycho Jungle Cat", 1994,),
-        ("There's Treasure Everywhere", 1996,),
-        ("It's a Magical World", 1996,),)
+nomatch = {}
+email_adresss = []
+email_adresss_dict = {}
+sheet = book.sheet_by_index(1)
+regstring = "[\-\_A-Za-z0-9]+[\.A-Za-z0-9]*@[\-A-Za-z0-9]+[\.a-z]+"
+#import pdb; pdb.set_trace()
 
+for counter in range(sheet.nrows):
+    rowValue = sheet.row_values(counter, start_colx=0, end_colx=1)[0]
+    try:
+        matchs = re.findall(regstring, str.strip(str(rowValue)))
+        if matchs:
+            for match in matchs:
+                email_adress = match
+                if email_adress not in email_adresss:
+                    email_adresss.append(str(email_adress))
+                    if len(matchs) > 1:
+                        full_rowValue = "Een mailinglijst"
+                    else:
+                        full_rowValue = str.strip(str(rowValue))
+                    email_adresss_dict[str(email_adress)] = [full_rowValue,
+                                                             str(email_adress).
+                                                             split("@")[0]]
+        else:
+            nomatch[str(counter)] = rowValue
+            #print "No match: ", rowValue, ", ",  counter
+    except UnicodeEncodeError:
+        matchs = re.findall(regstring, str.strip(rowValue.encode('ascii', 'replace')))
+        for match in matchs:
+            email_adress = match
+            if email_adress not in email_adresss:
+                email_adresss.append(str(email_adress))
+                if len(matchs) > 1:
+                    full_rowValue = "Een mailinglijst"
+                else:
+                    full_rowValue = (str.
+                                     strip(rowValue.encode('ascii',
+                                                            'replace')))
+                email_adresss_dict[str(email_adress)] = ([full_rowValue,
+                                                         str(email_adress).
+                                                         split("@")[0]])
 wb = xlwt.Workbook()
-ws = wb.add_sheet("My Sheet")
+ws = wb.add_sheet("Adressen wijkenbuurt jan 2013")
+ws.col(0).width = 256 * max([len(row) for row in email_adresss])
+ws.col(1).width = 256 * max([len(row) for row in email_adresss])
+ws.col(2).width = 256 * max([len(row) for row in email_adresss_dict])
+ws.col(3).width = 256 * max([len(row) for row in email_adresss_dict])
 
-# Add headings with styling and froszen first row
-heading_xf = xlwt.easyxf('font: bold on; align: wrap on, vert centre, horiz center')
-headings = ['Bill Watterson Books', 'Year Published']
-rowx = 0
-ws.set_panes_frozen(True) # frozen headings instead of split panes
-ws.set_horz_split_pos(rowx+1) # in general, freeze after last heading row
-ws.set_remove_splits(True) # if user does unfreeze, don't leave a split there
-for colx, value in enumerate(headings):
-    ws.write(rowx, colx, value, heading_xf)
 
-for i, row in enumerate(DATA):
-    for j, col in enumerate(row):
-        ws.write(i, j, col)
-ws.col(0).width = 256 * max([len(row[0]) for row in DATA])
-wb.save("myworkbook.xls")
+borders = xlwt.Borders()
+borders.left = xlwt.Borders.DASHED
+borders.right = xlwt.Borders.DASHED
+borders.top = xlwt.Borders.DASHED
+borders.bottom = xlwt.Borders.DASHED
+borders.left_colour = 0x40
+borders.right_colour = 0x40
+borders.top_colour = 0x40
+borders.bottom_colour = 0x40
+style = xlwt.XFStyle()
+style.borders = borders
+styles = dict(
+    bold='font: bold 1',
+    italic='font: italic 1',
+    # Wrap text in the cell
+    wrap_bold='font: bold 1; align: wrap 1;',
+    # White text on a blue background
+    reversed='pattern: pattern solid, fore_color blue; font: color white;',
+    # Light orange checkered background
+    light_orange_bg='pattern: pattern fine_dots, fore_color white, back_color orange;',
+    # Heavy borders
+    bordered='border: top thick, right thick, bottom thick, left thick;',
+    # 16 pt red text
+    big_red='font: height 320, color red;',
+)
+
+ws.write(0, 0, 'Volle naam', style)
+ws.write(0, 1, 'Gebruikers Naam', style)
+ws.write(0, 2, 'Mail adres', style)
+ws.write(0, 3, 'Oorspronkelijke cel inhoud', style)
+
+i = 1
+for email_adress in sorted(email_adresss, key=str.lower):
+    #print "<"+email_adress+">,"
+    sorted_email = "<"+email_adress+">,"
+    try:
+        ws.write(i, 1, email_adresss_dict[str(email_adress)][1])
+        ws.write(i, 3, email_adresss_dict[str(email_adress)][0])
+        full_name = str.strip(re.sub("[<]*"+regstring+"[>,;]*",
+                 '',
+                 email_adresss_dict[str(email_adress)][0]))
+        if full_name:
+            ws.write(i, 0, full_name)
+        else:
+            ws.write(i, 0, email_adresss_dict[str(email_adress)][1])
+    except KeyError:
+        print 'ooops'
+    se_link = 'HYPERLINK("mailto:'+sorted_email + '";"'+sorted_email+'")'
+    ws.write(i, 2, xlwt.Formula(se_link))
+    i += 1
+print "number of email:  ", len(email_adresss)
+wb.save("adr2.xls")
---
+++ merge_sort.py
@@ -0,0 +1,19 @@
+#!bin/python
+"""
+Klein scriptje om twee reeds gemanipuleerde xls te mergen
+en te sorteren.
+Kan natuurlijk uitgebreid worden tot een lijst van files.
+"""
+#TODO: Add specification for sheet number
+import xlrd
+import re
+import xlwt
+a_file = "adr.xls"
+a_book = (xlrd.open_workbook(a_file))
+a_sheet = a_book.sheet_by_index(0)
+b_file = "adr.xls"
+b_book = (xlrd.open_workbook(a_file))
+b_sheet = b_book.sheet_by_index(0)
+for counter in range(a_sheet.nrows):
+    rowValue = sheet.row_values(counter, start_colx=0, end_colx=1)[0]
+    print rowValue
